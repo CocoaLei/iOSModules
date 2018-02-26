c3c3d54e7aa885d784cae9f1332b1f4b8b6ee174
石城磊 <apple272487813@gmail.com>
2018-02-26 22:29:34 +0800

    Update video reorder

---
+++ iOSModules/Modules/IMVideoRecorder/Controllers/IMVideoInitialViewController.h
@@ -0,0 +1,13 @@
+//
+//  IMVideoInitialViewController.h
+//  iOSModules
+//
+//  Created by 石城磊 on 26/02/2018.
+//  Copyright © 2018 石城磊. All rights reserved.
+//
+
+#import "IMBaseViewController.h"
+
+@interface IMVideoInitialViewController : IMBaseViewController
+
+@end
---
+++ iOSModules/Modules/IMVideoRecorder/Controllers/IMVideoInitialViewController.m
@@ -0,0 +1,27 @@
+//
+//  IMVideoInitialViewController.m
+//  iOSModules
+//
+//  Created by 石城磊 on 26/02/2018.
+//  Copyright © 2018 石城磊. All rights reserved.
+//
+
+#import "IMVideoInitialViewController.h"
+
+@interface IMVideoInitialViewController ()
+
+@end
+
+@implementation IMVideoInitialViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+    self.title  =   @"IMVideoRecorder";
+    [self configureViewsApperance];
+}
+
+- (void)configureViewsApperance {
+}
+
+@end
